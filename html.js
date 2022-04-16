a05d5aeb4b525f733a3775326a866f679977e528
christh-ps <103792685+christh-ps@users.noreply.github.com>
2022-04-15 22:21:53 -0400

    initial commit

---
+++ html.js
@@ -0,0 +1,5 @@
+const navToggle = document.querySelector(".toggle")
+const navMenu = document.querySelector(".menu-lista")
+navToggle.addEventListener("click",()=>{
+    navMenu.classList.toggle("menu-visualizar")
+})
